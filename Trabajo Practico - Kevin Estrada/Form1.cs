9b2a2c200c5c0458e2e4f2afab1491f8d7837354
Quebin <Quebin@DESKTOP-L3IK1T5>
2023-04-30 00:44:30 -0300

    Test

---
+++ Trabajo Practico - Kevin Estrada/Form1.cs
@@ -0,0 +1,10 @@
+namespace Trabajo_Practico___Kevin_Estrada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
